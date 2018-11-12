11c9bad93274567e37d33aa5e632ae124d08cd93
Ivan Hrynevytskyy <ivantuer@gmail.com>
2018-11-12 09:55:45 +0200

    Updates

---
+++ precache-manifest.f41ae66be5e2093b7dc1458651daee1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "974d4f49fee31f14e61c",
+    "url": "/d2-chat/static/js/runtime~main.974d4f49.js"
+  },
+  {
+    "revision": "518072fc4fee38b1cab8",
+    "url": "/d2-chat/static/js/main.518072fc.chunk.js"
+  },
+  {
+    "revision": "1a0895c3b0c928d124c4",
+    "url": "/d2-chat/static/js/1.1a0895c3.chunk.js"
+  },
+  {
+    "revision": "518072fc4fee38b1cab8",
+    "url": "/d2-chat/static/css/main.92dc7ba6.chunk.css"
+  },
+  {
+    "revision": "69bb11e69307711d27f37128bdb876d4",
+    "url": "/d2-chat/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/d2-chat/precache-manifest.f41ae66be5e2093b7dc1458651daee1a.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/d2-chat/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
